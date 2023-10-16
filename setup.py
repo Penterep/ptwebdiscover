acf7e5c0873914453a8a91c1a7b867e0118eef9b
Daniel <dan@soom.cz>
2023-10-16 14:45:16 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
     ],
     python_requires = '>=3.6',
-    install_requires=["ptlibs>=1,<2", "requests", "appdirs", "tldextract>=3.6.0", "treelib"],
+    install_requires=["ptlibs>=1.0.3,<2", "requests", "appdirs", "tldextract>=5.0.0", "treelib"],
     entry_points = {'console_scripts': ['ptwebdiscover = ptwebdiscover.ptwebdiscover:main']},
     include_package_data= True,
     long_description=long_description,
